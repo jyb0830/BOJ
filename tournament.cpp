36cef61e5347e77f41f815229d5ccc3ad7fef30a
jyv0830 <wldudqhs@naver.com>
2020-12-29 23:56:00 +0900

    1057번 토너먼트

---
+++ tournament.cpp
@@ -0,0 +1,42 @@
+//
+//  tournament.cpp
+//  BOJ
+//
+//  Created by 지영본 on 2020/12/29.
+//  acmicpc.net/problem/1057
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main(){
+    int N, Kim, Lim, k=0;
+    
+    cin>>N>>Kim>>Lim;
+    
+    while(N>pow(2,k)) k++;
+    k--;
+    
+    for(int i=k;i>=0;i--){
+        if(Kim<=pow(2,i) && Lim<=pow(2,i))
+            continue;
+        
+        else if(Kim<=pow(2,i) && Lim > pow(2,i)){
+            cout<<i+1;
+            break;
+        }
+        else if(Kim>pow(2,i) && Lim<=pow(2,i)){
+            cout<<i+1;
+            break;
+        }
+        else if(Kim>pow(2,i) && Lim>pow(2,i)){
+            Kim=Kim%(int)pow(2,i);
+            Lim=Lim%(int)pow(2,i);
+            
+        }
+        
+    }
+    
+    
+    return 0;
+}
