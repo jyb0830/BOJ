aa681ed3535f161d7f496e9faf3323146efa9982
jyv0830 <wldudqhs@naver.com>
2021-02-12 23:53:17 +0900

    1991번 트리 순회

--- tree_traversal.cpp
+++ tree_traversal.cpp
@@ -16,12 +16,22 @@ private:
     TreeNode *LeftChild;
     char data;
     TreeNode *RightChild;
+public:
+    TreeNode(char data=0, TreeNode* left=0, TreeNode* right=0){
+        this->data=data;
+        this->LeftChild=left;
+        this->RightChild=right;
+    }
 };
 class Tree{
 public:
+    Tree(char data=0){
+        root= new TreeNode(data);
+    }
     void preorder(TreeNode *CurrentNode);
     void inorder(TreeNode *CurrentNode);
     void postorder(TreeNode *CurrentNode);
+    void insert(TreeNode *CurrentNode, char parent, char left, char right);
 private:
     TreeNode *root;
 };
@@ -50,10 +60,28 @@ void Tree::postorder(TreeNode *CurrentNode){
     }
 }
 
+void Tree::insert(TreeNode *CurrentNode, char parent, char left, char right){
+    if(CurrentNode->data==parent){
+        if(left!=0){
+        TreeNode* left_node=new TreeNode(left);
+        }
+        if(right!=0){
+        TreeNode* right_node=new TreeNode(right);
+        }
+    }
+}
+
 int main(){
     int n;
     cin>>n;
     
+    Tree tree('A');
+    
+    for(int i=0;i<n;i++){
+        char root,left,right;
+        cin>>root>>left>>right;
+        
+    }
     
     
     return 0;
