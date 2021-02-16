cd1a915a5f3c167578d26fcc0db4f9460d8ee490
jyv0830 <wldudqhs@naver.com>
2021-02-16 16:31:45 +0900

    11725번 트리의 부모찾기

--- find_parent.cpp
+++ find_parent.cpp
@@ -6,50 +6,50 @@
 //  acmicpc.net/problem/11725
 
 #include <iostream>
+#include <vector>
+#include <queue>
+
 using namespace std;
 
-int main(){
-    int n, cnt=0;
-    cin>>n;
-    int node1[n-1],node2[n-1];
-    int parent[n+1];
-    for(int i=0;i<n-1;i++){
-        cin>>node1[i]>>node2[i];
-    }
-    for(int i=0;i<n+1;i++) parent[i]=0;
+vector<vector<int>> v;
+bool visited[100001];
+queue<int> q;
+int parent[100001];
+
+void find_parent(){
+    visited[1]=true;
+    q.push(1);
     
-    while(cnt<n-1){
-        for(int i=0;i<n-1;i++){
-            if(node1[i]==1){    //연결된 노드 중 하나가 1인 경우 무조건 나머지 노드는 1의 자식
-                parent[node2[i]]=1;
-                node1[i]=node2[i]=0;
-                cnt++;
-                continue;
-            }
-            if(node2[i]==1){
-                parent[node1[i]]=1;
-                cnt++;
-                continue;
-            }
-            if(parent[node1[i]]==0&&parent[node2[i]]==0) continue;   //연결된 두 노드가 모두 부모가 없는 경우 대기
-            if(node1[i]==0 && node2[i]==0) continue;    //두 노드의 연결상태를 이미 반영했으면 통과
+    while(!q.empty()){
+        int temp = q.front();
+        q.pop();
+        
+        for(int i=0;i<v[temp].size();i++){
+            if(visited[v[temp][i]]) continue;
             
-            if(parent[node1[i]]!=0){
-                parent[node2[i]]=node1[i];
-                node1[i]=node2[i]=0;
-                cnt++;
-                continue;
-            }
-            else if(parent[node2[i]]!=0){
-                parent[node1[i]]=node2[i];
-                node1[i]=node2[i]=0;
-                cnt++;
-                continue;
+            else{
+                parent[v[temp][i]]=temp;
+                visited[v[temp][i]]=true;
+                q.push(v[temp][i]);
             }
         }
     }
+}
+
+int main(){
+    int n,node1,node2;
+    
+    cin>>n;
+    for(int i=0;i<n+1;i++) v.push_back(vector<int>());
+    
+    for(int i=0;i<n-1;i++){
+        cin>>node1>>node2;
+        v[node1].push_back(node2);
+        v[node2].push_back(node1);
+    }
+    
+    find_parent();
     
     for(int i=2;i<n+1;i++) cout<<parent[i]<<"\n";
     
-    return 0;
 }
