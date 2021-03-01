b04c8df33c04d51ebf76edd3a086e86f1629bc6c
jyv0830 <wldudqhs@naver.com>
2021-03-01 22:59:20 +0900

    1039번 교환

--- swap_digit.cpp
+++ swap_digit.cpp
@@ -4,112 +4,114 @@
 //
 //  Created by 지영본 on 2021/02/23.
 //  acmicpc.net/problem/1039
-
-#include <iostream>
-#include <vector>
-#include <algorithm>
-#include <cmath>
-#include <queue>
+//  문제점 : 큐에서 바꾼 수가 최대값일 때 빠져나오는 경우에서 문제가 발생한다.
+//         예를 들어 입력이 (199, 2)일 경우 첫 번째와 세 번째를 바꾸면 991이 돼서 빠져나온다. 이후 main문에서 남은 한 번을 바꿔 919가 나온다.
+//         하지만 199 -> 919 -> 991로 최대 값은 991이다.
+#include<iostream>
+#include<algorithm>
+#include<queue>
+#include<string>
+#include<set>
+#include<vector>
+ 
+#define endl "\n"
+#define MAX 1000000 + 1
 using namespace std;
-
-vector<int> v, max_v;
-queue<pair<int, vector<int>>> q;
-int k;
-
-vector<int> v_swap(vector<int> v, int a, int b){
-    int tmp=v[a];
-    v[a]=v[b];
-    v[b]=tmp;
-    return v;
-}
-
-bool v_same_check(vector<int> v1, vector<int> v2){
-    bool check=true;
-    for(int i=0;i<v1.size();i++){
-        if(v1[i]!=v2[i]){
-            check=false;
-            break;
-        }
+ 
+string Inp;
+int K, M;
+vector<int> Answer;
+ 
+int Invert(string S)
+{
+    int Sum = 0;
+    for (int i = 0; i < S.length(); i++)
+    {
+        Sum = Sum + (S[i] - '0');
+        if (i != S.length() - 1) Sum = Sum * 10;
     }
-    return check;
+    return Sum;
 }
-
-void bfs(){
-    q.push(make_pair(0, v));
-    
-    while(!q.empty()){
-        
-        if(q.front().first==k) return;
-        
-        if(v_same_check(max_v, q.front().second)) return;
-        
-        vector<int> temp=q.front().second;
-        int idx=q.front().first;
-        q.pop();
-        for(int i=(int)temp.size()-1;i>=0;i--){
-            if(temp[i]!=max_v[i]){
-                for(int j=i;j>=0;j--){
-                    if(temp[j]==max_v[i]){
-                        temp=v_swap(temp, i, j);
-                        if(temp[temp.size()-1]!=0) q.push(make_pair(idx+1, temp));
-                        temp=v_swap(temp, i, j);
+ 
+void Input()
+{
+    cin >> Inp >> K;
+    M = Inp.length();
+ 
+    if (M == 1 || (M == 2 && Invert(Inp) % 10 == 0))
+    {
+        cout << "-1" << endl;
+        exit(0);
+    }
+}
+ 
+void BFS(string S)
+{
+    queue<string> Q;
+    Q.push(S);
+    int Cur_K = 0;
+    int Max = 0;
+ 
+    while (Q.empty() == 0 && Cur_K < K)
+    {
+        int Qs = Q.size();
+        set<string> Visit;
+ 
+        for (int q = 0; q < Qs; q++)
+        {
+            string Cur = Q.front();
+            Q.pop();
+ 
+            for (int i = 0; i < M - 1; i++)
+            {
+                for (int j = i + 1; j < M; j++)
+                {
+                    if (i == 0 && Cur[j] == '0') continue;
+ 
+                    swap(Cur[i], Cur[j]);
+                    if (Visit.find(Cur) == Visit.end())
+                    {
+                        if (Cur_K == K - 1 && Max < Invert(Cur))
+                        {
+                            Max = Invert(Cur);
+                        }
+                        Q.push(Cur);
+                        Visit.insert(Cur);
                     }
+                    swap(Cur[i], Cur[j]);
                 }
-                break;
-            }
-            else{
-                continue;
             }
         }
+        Cur_K++;
     }
+ 
+    if (Cur_K != K) cout << "-1" << endl;
+    else cout << Max << endl;
+ 
 }
-
-int main(){
-    int num;
-    cin>>num>>k;
-    
-    while(num!=0){
-        v.push_back(num%10);
-        max_v.push_back(num%10);
-        num/=10;
-    }
-    sort(max_v.begin(), max_v.end());
-    
-    bfs();
-    int max_num=0;
-    
-    if(q.front().first==k){
-        while(!q.empty()){
-            int tmp_num=0;
-            vector<int> temp(q.front().second);
-            q.pop();
-            for(int i=(int)temp.size()-1;i>=0;i--){
-                tmp_num+=temp[i] * pow(10,i);
-            }
-            max_num=max(max_num, tmp_num);
-        }
-    }
-    else{
-        int count=0;
-        for(int i=0;i<max_v.size();i++){
-            if(max_v[i]!=0) count++;
-        }
-        
-        if(count<=1) {
-            cout<<"-1\n";
-            return 0;
-        }
-        
-        if((k-q.front().first)%2!=0){
-            max_v=v_swap(max_v, 0, 1);
-        }
-        
-        max_num=0;
-        for(int i=(int)max_v.size()-1;i>=0;i--){
-            max_num+=max_v[i]*pow(10,i);
-        }
-    }
-    
-    cout<<max_num<<"\n";
+ 
+void Solution()
+{
+    BFS(Inp);
+}
+ 
+void Solve()
+{
+    Input();
+    Solution();
+}
+ 
+int main(void)
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+ 
+    //freopen("Input.txt", "r", stdin);
+    Solve();
+ 
     return 0;
 }
+
+
+출처: https://yabmoons.tistory.com/152 [얍문's Coding World..]
