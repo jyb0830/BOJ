970f93657fc91faa1f40e1e6806622732eff7ec7
jyv0830 <wldudqhs@naver.com>
2021-03-21 18:00:13 +0900

    1060번 좋은수

--- good_number.cpp
+++ good_number.cpp
@@ -10,65 +10,81 @@
 #include <algorithm>
 using namespace std;
 
+int64_t max_num=9223372036854775807;
+
 int main(){
     int l,n;
     cin>>l;
-    
-    int arr[l+1], cnt=0;
-    vector<pair<int,int>> v, sector, good, answer;
+    int64_t arr[l+1], cnt=0;
+    vector<pair<int64_t, int64_t>> v, sector, good, answer;
     arr[0]=0;
     for(int i=1;i<l+1;i++){
         cin>>arr[i];
-        cout<<"arr["<<i<<"] : "<<arr[i]<<"\n";
         answer.push_back(make_pair(0, arr[i]));
         cnt++;
     }
     cin>>n;
     for(int i=0;i<l;i++){
+        if(arr[i+1]-arr[i]==1) {
+            continue;
+        }
+        if(arr[i+1]-arr[i]==2){
+            answer.push_back(make_pair(0, arr[i+1]-1));
+            cnt++;
+            continue;
+        }
         v.push_back(make_pair(arr[i]+1, arr[i+1]-1));
         good.push_back(make_pair(arr[i+1]-arr[i]-2, 0));
     }
     for(int i=0;i<v.size();i++){
         sector.push_back(make_pair(arr[i+1]-arr[i]-2, i));
     }
-    
-    while(cnt<n){
-        sort(sector.begin(), sector.end());
-        int idx = sector[0].second;
-        cout<<"크기 : "<<sector[0].first<<"  ";
-        cout<<"idx : "<<idx<<"\n";
-        int size = v[idx].second-v[idx].first+1;
-        if(good[idx].second==(v[idx].second-v[idx].first+1)/2){
-            cout<<"들어옴\n";
-            if((v[idx].second-v[idx].first+1)%2==1){
-                answer.push_back(make_pair(good[idx].first, v[idx].first+good[idx].second));
-                good[idx].first=2100000000;
-                cnt++;
-                continue;
+    if(!sector.empty()){
+        while(cnt<n){
+            sort(sector.begin(), sector.end());
+            int64_t idx = sector[0].second;
+            if(sector[0].first==max_num){
+                break;
+            }
+            
+            int64_t size = v[idx].second-v[idx].first+1;
+            if(good[idx].second==(v[idx].second-v[idx].first)/2){
+                if((v[idx].second-v[idx].first+1)%2==1){
+                    answer.push_back(make_pair(good[idx].first, v[idx].first+good[idx].second));
+                    good[idx].first=max_num;
+                    sector[0].first=good[idx].first;
+                    cnt++;
+                    continue;
+                }
+                else{
+                    answer.push_back(make_pair(good[idx].first, v[idx].first+good[idx].second));
+                    answer.push_back(make_pair(good[idx].first, v[idx].second-good[idx].second));
+                    good[idx].first=max_num;
+                    sector[0].first=good[idx].first;
+                    cnt+=2;
+                    continue;
+                }
             }
             else{
                 answer.push_back(make_pair(good[idx].first, v[idx].first+good[idx].second));
                 answer.push_back(make_pair(good[idx].first, v[idx].second-good[idx].second));
-                good[idx].first=2100000000;
+                good[idx].second++;
+                good[idx].first= good[idx].second * (size-good[idx].second) + (size-good[idx].second-1);
+                sector[0].first=good[idx].first;
                 cnt+=2;
-                continue;
             }
         }
-        else{
-            answer.push_back(make_pair(good[idx].first, v[idx].first+good[idx].second));
-            answer.push_back(make_pair(good[idx].first, v[idx].second-good[idx].second));
-            good[idx].second++;
-            good[idx].first= good[idx].second * (size-good[idx].second) + (size-good[idx].second-1);
-            sector[0].first=good[idx].first;
-            cnt+=2;
-        }
     }
-    
+    int plus=1;
+    while(cnt<n){
+        answer.push_back(make_pair(max_num, arr[l]+plus));
+        plus++;
+        cnt++;
+    }
     sort(answer.begin(), answer.end());
     for(int i=0;i<n;i++){
         cout<<answer[i].second<<" ";
     }
-    cout<<"\n";
     
     return 0;
 }
