b3919d27c01c31c2b8a62ff78cab3227ee39ae57
jyv0830 <wldudqhs@naver.com>
2021-05-22 16:06:24 +0900

    1060번 좋은수 #우선순위 큐 #메모리초과

--- good_number.cpp
+++ good_number.cpp
@@ -2,88 +2,86 @@
 //  good_number.cpp
 //  BOJ
 //
-//  Created by 지영본 on 2021/03/19.
+//  Created by 지영본 on 2021/04/29.
 //  acmicpc.net/problem/1060
 
 #include <iostream>
+#include <queue>
 #include <vector>
 #include <algorithm>
 using namespace std;
 
-int64_t max_num=9223372036854775807;
+struct compare{
+    bool operator()(pair<int, int> a, pair<int,int> b){
+        return a.second>b.second;
+    }
+};
 
 int main(){
-    int l,n;
-    cin>>l;
-    int64_t arr[l+1], cnt=0;
-    vector<pair<int64_t, int64_t>> v, sector, good, answer;
-    arr[0]=0;
-    for(int i=1;i<l+1;i++){
-        cin>>arr[i];
-        answer.push_back(make_pair(0, arr[i]));
-        cnt++;
+//    priority_queue<pair<int,int>, vector<pair<int, int>>, compare> pq;
+    int L,n;
+    
+    cin>>L;
+    
+    vector<int> v, answer;
+    v.push_back(0);
+    for(int i=0;i<L;i++){
+        int num;
+        cin>>num;
+        v.push_back(num);
     }
     cin>>n;
-    for(int i=0;i<l;i++){
-        if(arr[i+1]-arr[i]==1) {
+    
+    for(int i=1;i<L+1;i++){                 //집합 S의 원소 정답벡터에 push
+        answer.push_back(v[i]);
+    }
+    for(int i=0;i<L;i++){
+        if(v[i+1]-v[i]==2)
+            answer.push_back(v[i]+1);       //구간의 갯수가 0인 수 정답벡터에 push
+    }
+    sort(answer.begin(), answer.end());     //정답벡터 정렬
+    
+    if(answer.size()>=n){                   //
+        for(int i=0;i<n;i++){
+            cout<<answer[i]<<" ";
+        }
+        return 0;
+    }
+    
+    vector<pair<int64_t,int>> p_v;
+    
+    for(int i=0;i<L;i++){
+        int size=v[i+1]-v[i]-1;
+        if(size==1)
             continue;
-        }
-        if(arr[i+1]-arr[i]==2){
-            answer.push_back(make_pair(0, arr[i+1]-1));
-            cnt++;
-            continue;
-        }
-        v.push_back(make_pair(arr[i]+1, arr[i+1]-1));
-        good.push_back(make_pair(arr[i+1]-arr[i]-2, 0));
-    }
-    for(int i=0;i<v.size();i++){
-        sector.push_back(make_pair(arr[i+1]-arr[i]-2, i));
-    }
-    if(!sector.empty()){
-        while(cnt<n){
-            sort(sector.begin(), sector.end());
-            int64_t idx = sector[0].second;
-            if(sector[0].first==max_num){
-                break;
-            }
-            
-            int64_t size = v[idx].second-v[idx].first+1;
-            if(good[idx].second==(v[idx].second-v[idx].first)/2){
-                if((v[idx].second-v[idx].first+1)%2==1){
-                    answer.push_back(make_pair(good[idx].first, v[idx].first+good[idx].second));
-                    good[idx].first=max_num;
-                    sector[0].first=good[idx].first;
-                    cnt++;
-                    continue;
-                }
-                else{
-                    answer.push_back(make_pair(good[idx].first, v[idx].first+good[idx].second));
-                    answer.push_back(make_pair(good[idx].first, v[idx].second-good[idx].second));
-                    good[idx].first=max_num;
-                    sector[0].first=good[idx].first;
-                    cnt+=2;
-                    continue;
-                }
-            }
-            else{
-                answer.push_back(make_pair(good[idx].first, v[idx].first+good[idx].second));
-                answer.push_back(make_pair(good[idx].first, v[idx].second-good[idx].second));
-                good[idx].second++;
-                good[idx].first= good[idx].second * (size-good[idx].second) + (size-good[idx].second-1);
-                sector[0].first=good[idx].first;
-                cnt+=2;
-            }
+        for(int j=v[i]+1;j<v[i+1];j++){
+            int sec_num=(j-v[i]-1)*(v[i+1]-j)+v[i+1]-j-1;
+            p_v.push_back(make_pair(sec_num, j));
         }
     }
-    int plus=1;
-    while(cnt<n){
-        answer.push_back(make_pair(max_num, arr[l]+plus));
-        plus++;
-        cnt++;
+    sort(p_v.begin(),p_v.end());
+
+    if(n<=v[v.size()-1]){
+        int s=(int)answer.size();
+        for(int i=0;i<n-s;i++){
+            answer.push_back(p_v[i].second);
+        }
     }
-    sort(answer.begin(), answer.end());
-    for(int i=0;i<n;i++){
-        cout<<answer[i].second<<" ";
+    
+    else{
+        for(int i=0;i<p_v.size();i++){
+            answer.push_back(p_v[i].second);
+        }
+        
+        n=n-v[v.size()-1];
+        
+        for(int i=1;i<=n;i++){
+            answer.push_back(v[v.size()-1]+i);
+        }
+    }
+
+    for(int i=0;i<answer.size();i++){
+        cout<<answer[i]<<" ";
     }
     
     return 0;
